a6439a31bc0dd878bad01721690797d44fa8b4c2
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-11-22 14:48:31 +0100

    REST API con MongoDB e Apache Kafka!

---
+++ src/main/java/com/soprasteria/springboottestmongodb/SpringboottestmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.soprasteria.springboottestmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottestmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottestmongodbApplication.class, args);
+	}
+
+}
