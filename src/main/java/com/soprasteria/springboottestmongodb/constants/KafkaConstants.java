97a99482c2bcec4c95027a8f62c25cc150342d9e
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-11-28 14:12:02 +0100

    nuovi commenti

--- src/main/java/com/soprasteria/springboottestmongodb/constants/KafkaConstants.java
+++ src/main/java/com/soprasteria/springboottestmongodb/constants/KafkaConstants.java
@@ -2,6 +2,8 @@ package com.soprasteria.springboottestmongodb.constants;
 
 public class KafkaConstants {
 	
+	//COSTANTI RELATIVE ALL'UTILIZZO DI KAFKA
+	
 	public static final String KAFKA_SERVER_CONFIG = "localhost:9092";
 	public static final String TOPIC_NAME = "kafkaTopicStudenti";
 	public static final String KAFKA_LISTENER_CONTAINER_FACTORY = "kafkaListenerContainerFactory";
